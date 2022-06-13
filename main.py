31f497069efdbfb7846c07094306ebbe81b3cb84
Amos Kipyegon <amoskip@kabarak.ac.ke>
2022-06-13 16:07:00 +0300

    project setup and schemas

---
+++ main.py
@@ -0,0 +1,21 @@
+from typing import List
+import fastapi as _fastapi
+import sqlalchemy.orm as _orm
+import v1.services as _services
+import core.schemas.schemas as _schemas
+
+app = _fastapi.FastAPI()
+
+_services.create_database()
+
+@app.post("/users/", response_model=_schemas.User)
+def create_user(user: _schemas.UserCreate, db: _orm.Session=_fastapi.Depends(_services.get_db)):
+    db_user = _services.get_user_by_email(db=db, email=user.email)
+    if db_user:
+        raise _fastapi.HTTPException(status_code=400, detail="email is in use")
+    return _services.create_user(db=db, user=user)
+
+@app.get("/users/", response_model=List[_schemas.User])
+def read_users(skip: int=0, limit: int=10, db:_orm.Session=_fastapi.Depends(_services.get_db)):
+    users = _services.get_users(db=db, skip=skip, limit=limit)
+    return users
---
+++ v1/services.py
@@ -0,0 +1,30 @@
+import email
+import sqlalchemy.orm as _orm
+
+import core.models.databases as _database
+import core.models.models as _models
+import core.schemas.schemas as _schemas
+
+def create_database():
+    return _database.Base.metadata.create_all(bind=_database.engine)
+
+def get_db():
+    db = _database.SessionalLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+def get_user_by_email(db: _orm.Session, email:str):
+    return db.query(_models.User).filter(_models.User.email == email).first()
+
+def create_user(db: _orm.Session, user: _schemas.UserCreate):
+    fake_password = user.password + "notsecure"
+    db_user = _models.User(email=user.email, hashed_password=fake_password)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+def get_users(db:_orm.Session, skip: int, limit: int):
+    return db.query(_models.User).offset(skip).limit(limit).all()
